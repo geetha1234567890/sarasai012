5dbbc72be8bbf26c690773ca2b509125577c603e
geetha1234567890 <60750875+geetha1234567890@users.noreply.github.com>
2024-08-08 11:56:23 +0530

    Update jest.config.js

--- SarasAI-Repo-1/SARAS_AI_FRONTEND/jest.config.js
+++ SarasAI-Repo-1/SARAS_AI_FRONTEND/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Handle JS, JSX, TS, and TSX files
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Transform JS, JSX, TS, and TSX files
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(axios|redux-toolkit|react-toastify)/)' // Include specific modules for transformation
+    '/node_modules/(?!(axios|redux-toolkit|react-toastify)/)' // Transform specific node_modules
   ],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
