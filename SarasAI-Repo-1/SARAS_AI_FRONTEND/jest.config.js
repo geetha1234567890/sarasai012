28047cdc98509cb9ab1924e887d1968040f1424d
geetha1234567890 <60750875+geetha1234567890@users.noreply.github.com>
2024-08-11 00:16:46 +0530

    Update jest.config.js

--- SarasAI-Repo-1/SARAS_AI_FRONTEND/jest.config.js
+++ SarasAI-Repo-1/SARAS_AI_FRONTEND/jest.config.js
@@ -1,9 +1,12 @@
-// jest.config.js
 module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(axios)/)" // Add exceptions for modules like axios
+    '/node_modules/(?!(axios)/)'  // Process ES module dependencies like axios
   ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',  // Mock CSS imports
+  },
+  testEnvironment: 'jsdom',
 };
