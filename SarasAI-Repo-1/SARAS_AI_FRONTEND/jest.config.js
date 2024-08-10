cdd46b46fa3612d5db3f6245a58ea42a69c60c2a
geetha1234567890 <60750875+geetha1234567890@users.noreply.github.com>
2024-08-11 01:07:49 +0530

    Update jest.config.js

--- SarasAI-Repo-1/SARAS_AI_FRONTEND/jest.config.js
+++ SarasAI-Repo-1/SARAS_AI_FRONTEND/jest.config.js
@@ -1,24 +1,32 @@
-module.exports = {
-  preset: "jest-expo",
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
-  },
-  globals: {
-    "ts-jest": {
-      transformerConfig: {
-        transformIgnorePatterns: [
-          "<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)",
-          "jest-runner",
-        ],
-      },
-    },
-  },
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/.maestro/",
-    "@react-native",
-  ],
-  testEnvironment: "react-native",
-  setupFiles: ["<rootDir>/test/setup.ts"],
-  setupFilesAfterEnv: ["<rootDir>/jest-setup-after-env.js"],
+const nextJest = require('next/jest');
+const tsconfig = require('./tsconfig.json');
+const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig);
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  
+
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  bail: true,
+  logHeapUsage: true,
+  testTimeout: 120000,
+  forceExit: true,
+  collectCoverage: true,
+
+
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  moduleNameMapper,
+  
 };
+
+
+module.exports = createJestConfig(customJestConfig);
